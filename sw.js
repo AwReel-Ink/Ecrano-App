0eda9fbd1e649764f9bd120c6820bc3d9fbc89a7
AwReel-Ink <awreeldroid@gmail.com>
2025-11-16 01:43:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ecrano-v1.1.0';
+const CACHE_NAME = 'ecrano-v1.2.1';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -59,3 +59,4 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
